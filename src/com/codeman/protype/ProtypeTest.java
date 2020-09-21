721cb8ea69f2c978a5a78346aad991b04f2e75dd
彭阳宴 <531639577@qq.com>
2020-09-21 09:48:48 +0800

    原型模式

--- src/com/codeman/protype/ProtypeTest.java
+++ src/com/codeman/protype/ProtypeTest.java
@@ -1,14 +1,24 @@
 package com.codeman.protype;
 
-import java.io.Closeable;
+import java.io.*;
 
 /**
- * 原型模式
+ * 原型模式  clone
  * @author pengyangyan
  * @version 1.0.0
  * @date 2020年09月18日 10:10:00
  */
-public class ProtypeTest implements Cloneable {
+public class ProtypeTest implements Cloneable,Serializable{
+
+    private String str = "小米";
+
+    private Student student;
+
+    public ProtypeTest(String s,Student student){
+        super();
+        this.str = s;
+        this.student = student;
+    }
 
     //通过clone获取
     @Override
@@ -19,9 +29,40 @@ public class ProtypeTest implements Cloneable {
 
     //深拷贝和浅拷贝
 
+    public Object deepClone() throws IOException, ClassNotFoundException, CloneNotSupportedException {
+        super.clone();
+        ByteArrayOutputStream opt = new ByteArrayOutputStream();
+        ObjectOutputStream obj = new ObjectOutputStream(opt);
+        obj.writeObject(this);
 
+        ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(opt.toByteArray());
+        ObjectInputStream ois = new ObjectInputStream(byteArrayInputStream);
+        return ois.readObject();
+    }
 
+    public String getStr() {
+        return str;
+    }
 
+    public void setStr(String str) {
+        this.str = str;
+    }
+
+    public Student getStudent() {
+        return student;
+    }
+
+    public void setStudent(Student student) {
+        this.student = student;
+    }
+
+    @Override
+    public String toString() {
+        return "ProtypeTest{" +
+                "str='" + str + '\'' +
+                ", student=" + student +
+                '}';
+    }
 }
 
 
